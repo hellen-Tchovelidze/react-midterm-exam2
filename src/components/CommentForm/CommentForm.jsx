8caf9e2bdbbd44663f998d45832f5e500343e778
hellen-Tchovelidze <elenechovelidze@gmail.com>
2025-02-19 18:37:53 +0400

    change

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import system from "../../assets/Images/sysytem.png"
 const CommentForm = ({ newComment, setNewComment, addComment }) => {
   return (
-    <div className="flex justify-center items-center bg-white max-w-[730px] w-full h-36 max-h-full cursor-pointer rounded-lg gap-1.5">
+    <div className="flex justify-center items-center bg-white w-[730px]  h-36  cursor-pointer rounded-lg gap-1.5">
         <div><img src={system} alt="" /></div>
       <textarea
         type="text"
         placeholder="Write a comment..."
         value={newComment}
         onChange={(e) => setNewComment(e.target.value)}
-        className="w-full max-w-[506px] h-[96px] border-2 border-[#E9EBF0] focus:border-[#5357B6] focus:outline-none rounded-lg p-2"
+        className="w-[506px] h-[96px] border-2 border-[#E9EBF0] focus:border-[#5357B6] focus:outline-none rounded-lg p-2"
       ></textarea>
       <button
         onClick={addComment}
